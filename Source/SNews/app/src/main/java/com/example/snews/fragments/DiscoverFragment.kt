44fac47a36fd98e80b9eb0ddc4f00d118ca8d673
Samuel Netherway <samuelnetherwaygit@gmail.com>
2020-11-15 16:16:00 +0000

    Added discover UI ready for firebase storage of user selected categories.

--- Source/SNews/app/src/main/java/com/example/snews/fragments/DiscoverFragment.kt
+++ Source/SNews/app/src/main/java/com/example/snews/fragments/DiscoverFragment.kt
@@ -1,18 +1,20 @@
 package com.example.snews.fragments
 
-import android.content.Context.MODE_PRIVATE
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.snews.R
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class DiscoverFragment : Fragment() {
 
-    private val sharedPrefFile = "com.example.snews.discoverprefs"
+    //private val sharedPrefFile = "com.example.snews.discoverprefs"
+
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +27,9 @@ class DiscoverFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        addData()
+
+        /*
         var discoverPreferences = activity!!.getSharedPreferences(sharedPrefFile, MODE_PRIVATE)
         val preferencesEditor: SharedPreferences.Editor = discoverPreferences!!.edit()
         //preferencesEditor.putString("test", "success")
@@ -34,5 +39,16 @@ class DiscoverFragment : Fragment() {
         println(discoverPreferences.getString("test", "empty"))
         println(discoverPreferences.getString("test", "empty"))
         println(discoverPreferences.getString("test", "empty"))
+        */
+    }
+
+    fun addData() {
+        val userOne: MutableMap<String, Any> = HashMap()
+        userOne["first_name"] = "Martin"
+        userOne["last_name"] = "Netherway"
+        userOne["favourite_topics"] = arrayListOf("Health", "Science", "Politics")
+
+        // Add a new document with a generated ID
+        db.collection("users").add(userOne)
     }
 }
